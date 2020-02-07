36ca53e4dd504913351199f471a74b234e9b9e7a
Orhan Rosenfeld <azzan.rosenfedl@outlook.com>
2020-02-07 22:20:56 +0100

    [UPDATE] :: Database connection has been set.

--- ProjectIO/settings.py
+++ ProjectIO/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'ProjectIO.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'projectio',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model) :
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    post_date = models.DateTimeField(default = timezone.now) #auto_now = True #auto_now_add = True
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
