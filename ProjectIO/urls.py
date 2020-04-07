0bf9c8c086f12f739266d9d4ca003a27dc88de8f
Orhan Rosenfeld <azzan.rosenfeld@outlook.com>
2020-04-07 21:46:50 +0200

    [UPDATE] : Welcome/Landing page has been removed just to be added to a new project later.

--- ProjectIO/urls.py
+++ ProjectIO/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('blog.urls')),
-    path('welcome/', include('welcome.urls'))
+    path('home/', include('blog.urls'))
 ]
--- welcome/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-           
-def signup(request) :
-    return render(request, 'welcome/signup.html')
-
-def index(request) :
-    return render(request, 'welcome/index.html')
